6b2a7b3560bb9539cabfc32c007df1f36273f64b
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-02-19 23:56:41 +0300

    Category page edited.

--- BlogMVCApp/Controllers/CategoryController.cs
+++ BlogMVCApp/Controllers/CategoryController.cs
@@ -53,7 +53,7 @@ namespace BlogMVCApp.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,CategoryName")] Category category)
+        public ActionResult Create([Bind(Include = "CategoryName")] Category category)
         {
             if (ModelState.IsValid)
             {
